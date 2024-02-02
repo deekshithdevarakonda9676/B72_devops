80250612d0d06507b9c35a96c3b9d22b58a9ce12
deekshithdevarakonda9676 <dhonideekshith7@gmail.com>
2024-02-02 23:25:18 +0530

    developer 2 added new code

--- application.java
+++ application.java
@@ -26,3 +26,22 @@ public static void main(String args[])
  }    
   
 }}  
+
+#developer  2 added new code changes from here
+class PalindromeExample{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
