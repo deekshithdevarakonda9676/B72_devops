395b608bb93323b587649578f2ab3a2f0022d123
deekshithdevarakonda9676 <dhonideekshith7@gmail.com>
2024-02-02 23:19:26 +0530

    new changes made by developer1

--- application.java
+++ application.java
@@ -9,3 +9,20 @@ int n = sc.nextInt();
 int reverse_number = reverseNumber(n);   
 System.out.println("The reverse of the given number is: "+reverse_number);   
 }  
+#developer 1 added new code from here
+
+class FibonacciExample1{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}  
